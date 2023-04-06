8da47f402f2d144fdfc7c946fe792d9dd2dd0137
simao <lewisboss88@gmail.com>
2023-04-06 17:11:18 +0100

    webdevelopment page first interraction 2 navbar

---
+++ webdevitems.js
@@ -0,0 +1,25 @@
+
+function previewOpenMenu(i){
+    const menuP = document.getElementsByClassName("previewMenu");
+    switch(i){
+        case 0:
+            menuP[0].style.transform = "scale(0)";
+        break;
+        case 1: 
+            menuP[0].style.transform = "scale(1)";
+        break;
+        case 2:
+            document.querySelector(".faceItem").classList.toggle("changeMenu");
+
+    }
+}
+
+function previewMenuOpenClose(){
+    document.querySelector(".faceItem2").classList.toggle("changeWraper");
+    if(document.getElementById("previewOpenWraper").getAttribute("class") == "fas fa-bars"){
+        document.getElementById("previewOpenWraper").setAttribute("class", "fas fa-x");
+    }else document.getElementById("previewOpenWraper").setAttribute("class", "fas fa-bars");
+    
+    
+
+}
